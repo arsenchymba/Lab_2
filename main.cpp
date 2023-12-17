b3d4b709c1e6671110b8a0ec9cc9783e1001524c
arsenchymba <146808087+arsenchymba@users.noreply.github.com>
2023-12-17 20:24:38 +0700

    Add files via upload
    Считываю строки из файла in.txt с неким количеством строк с курсом валют. 
    Разделяю считанные строки на названия валют (строки), курс валют (числа с запятой) и дату (дата).
    Вывожу на консоль названия валюты №1 и №2, их курс, дату

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+#include <QDate>
+
+
+class currency {
+public:
+
+    QString name1;
+    QString name2;
+
+    float course;
+    QDate date;
+
+};
+
+// Проверяет, является ли строка s датой, и если да - преобразует ее в объект QDate
+
+bool isDate(const QString& s, QDate& dt) {
+    dt = QDate::fromString(s, "dd.MM.yyyy"); // Преобразует строку в объект QDate, используя формат "день.месяц.год"
+    return dt.isValid(); // Возвращает true, если объект QDate содержит действительную дату
+}
+
+// Проверяет, является ли строка s числом с плавающей точкой и, если да - преобразует ее в тип float
+bool isFloat(const QString& s, float& num) {
+    bool ok;
+    // Преобразует строку в тип float и устанавливает флаг ok в true, если преобразование прошло успешно
+    num = s.toFloat(&ok);
+    return ok; // Возвращает true, если преобразование строки в тип float прошло успешно
+}
+
+
+int main()
+{
+    QFile file("in.txt");
+
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(&file); // Создаем объект QTextStream для чтения данных из файла построчно
+
+        // В цикле читаем строки из файла
+        while (!in.atEnd()) {
+            QString line = in.readLine(); // Читаем строку из файла
+            QStringList parts = line.split(" "); // Разделяем строку на подстроки по пробелам
+
+            currency curr; // Создаем объект класса "currency"
+
+            for (const QString &part : parts) {
+                float num;
+                QDate dt;
+
+                // Проверяем, является ли подстрока числом с плавающей точкой
+                if (isFloat(part, num)) {
+                    curr.course = num; // Если да, сохраняем это число в поле "course" объекта "currency"
+
+                } else if (isDate(part, dt)) { // Если подстрока является датой
+                    curr.date = dt;
+                } else {
+                    // Отделяем другие части строки, которые могут быть названиями валют
+                    if (curr.name1.isEmpty())
+                        curr.name1 = part;
+                    else if (curr.name2.isEmpty())
+                        curr.name2 = part;
+                }
+            }
+
+             qDebug() << "Валюта 1:" << curr.name1 << "Валюта 2:" << curr.name2 << "Курс:" << curr.course << "Дата:" << curr.date.toString("yyyy.MM.dd");
+        }
+
+    } else {
+        qDebug() << "Не удалось открыть файл";
+    }
+
+    return 0;
+}
